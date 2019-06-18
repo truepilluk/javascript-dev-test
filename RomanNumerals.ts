d24bb0780096470bf5050cb37456d9e58522007d
Daniel Clegg <dan@truepill.com>
2019-06-18 09:22:06 +0100

    test

--- RomanNumerals.ts
+++ RomanNumerals.ts
@@ -4,7 +4,33 @@ interface RomanNumeralGenerator {
 
 class RomanNumerals implements RomanNumeralGenerator {
     generate(num: number) {
-        return "I";
+        var digits = num
+            .toString()
+            .split("")
+            .reverse();
+        var res = "";
+        var i = 1;
+        digits.forEach(function(n) {
+            var no = parseInt(n);
+            if (i == 1) {
+                if (no < 4) {
+                    res += "I";
+                }
+                if (no == 4) {
+                    res += "IV";
+                }
+                if (no == 5) {
+                    res += "V";
+                }
+            }
+            if (i == 2) {
+                if (no == 1) {
+                    res += "X";
+                }
+            }
+            i += 1;
+        });
+        return res;
     }
 }
 
