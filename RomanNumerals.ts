2ba09f2e53d8791afca27c90bc1af724f4d11ddf
Daniel Clegg <dc@danielclegg.com>
2019-11-06 11:25:34 +0000

    removed old candidates work

--- RomanNumerals.ts
+++ RomanNumerals.ts
@@ -4,33 +4,7 @@ interface RomanNumeralGenerator {
 
 class RomanNumerals implements RomanNumeralGenerator {
     generate(num: number) {
-        var digits = num
-            .toString()
-            .split("")
-            .reverse();
-        var res = "";
-        var i = 1;
-        digits.forEach(function(n) {
-            var no = parseInt(n);
-            if (i == 1) {
-                if (no < 4) {
-                    res += "I";
-                }
-                if (no == 4) {
-                    res += "IV";
-                }
-                if (no == 5) {
-                    res += "V";
-                }
-            }
-            if (i == 2) {
-                if (no == 1) {
-                    res += "X";
-                }
-            }
-            i += 1;
-        });
-        return res;
+        if (num === 1) return "I";
     }
 }
 
--- tests/test.ts
+++ tests/test.ts
@@ -9,23 +9,5 @@ describe("Roman Numeral Generator tests", function() {
 
             assert.equal(generator.generate(1), "I");
         });
-
-        it("should return 'IV", () => {
-            var generator = new RomanNumerals();
-
-            assert.equal(generator.generate(4), "IV");
-        });
-
-        it("should return 'V", () => {
-            var generator = new RomanNumerals();
-
-            assert.equal(generator.generate(5), "V");
-        });
-
-        it("should return X", () => {
-            var generator = new RomanNumerals();
-
-            assert.equal(generator.generate(10), "X");
-        });
     });
 });
